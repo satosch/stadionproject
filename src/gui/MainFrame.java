ef48b3dc251b00eeec76a97e40dcb12f28a40788
satoshin <satoshinks@gmail.com>
2021-06-24 14:40:40 +0200

    second commit

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,6 +1,9 @@
 // MainFrame für die alle Panels
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -9,19 +12,20 @@ public class MainFrame extends JFrame{
 	public String[] PanelNames = {"mp","sp","pp"};
 	Mainpanel mp = new Mainpanel(this, PanelNames[0]);
 	Loginhome sp = new Loginhome(this,PanelNames[1]);
-//	Seat pp = new Seat(this,PanelNames[2]);
+	Seat pp = new Seat(this,PanelNames[2]);
 //	Anmeldung rgst = new Anmeldung(this,PanelNames[2]);
 	
 	public MainFrame(){
-		this.add(mp);
+		this.setLayout(new BorderLayout());
+		this.add(mp,BorderLayout.NORTH);
 		mp.setVisible(true);
-		this.add(sp);
+		this.add(sp,BorderLayout.CENTER);
 		sp.setVisible(false);
-//		this.add(pp);
-//		pp.setVisible(false);
+		this.add(pp,BorderLayout.SOUTH);
+		pp.setVisible(false);
 //		this.add(rgst);
 //		rgst.setVisible(false);
-		this.setBounds(100, 100, 1000, 1000);
+		this.setSize(1500, 1000);
 		
 	}
 	
@@ -34,6 +38,7 @@ public class MainFrame extends JFrame{
 	public void PanelChange(JPanel jp, String str){
 		
 		System.out.println(jp.getName());
+		System.out.println(str);
 		String name = jp.getName();
 		
 		if(name==PanelNames[0]){
@@ -43,25 +48,27 @@ public class MainFrame extends JFrame{
 			sp = (Loginhome)jp;
 			sp.setVisible(false);
 		}
-//		else if(name==PanelNames[2]){
-//			pp = (Seat)jp;
-//			pp.setVisible(false);
+		else if(name==PanelNames[2]){
+			pp = (Seat)jp;
+			pp.setVisible(false);
 //		else if(name==PanelNames[2]){
 //			rgst = (Anmeldung)jp;
 //			rgst.setVisible(false);
-//		}
-		
+		}
 		if(str==PanelNames[0]){
 			mp.setVisible(true);
+			System.out.println(PanelNames[0]);
 		}
 		else if(str==PanelNames[1]){
 			sp.setVisible(true);
+			System.out.println(PanelNames[1]);
 		}
-//		else if(str==PanelNames[2]){
-//			pp.setVisible(true);
-//		}
+		else if(str==PanelNames[2]){
+			pp.setVisible(true);
+			System.out.println(PanelNames[2]);
+		}
 //		else if (str==PanelNames[2]) {
 //			rgst.setVisible(true);
 //		}
+		}
 	}
-}
--- src/gui/Seat.java
+++ src/gui/Seat.java
@@ -8,78 +8,104 @@ import javax.swing.border.*;
 public class Seat extends JPanel {
 	MainFrame mf;
 	String str;
-//    
-////	private JToggleButton[] seats = new JToggleButton[500];
-////    private JTextArea selectedSeats = new JTextArea(3, 40);
-//	
+    
+	private JToggleButton[] seats = new JToggleButton[500];
+    private JTextArea selectedSeats = new JTextArea(3, 40);
+	
     public Seat(MainFrame m, String s) {
     	mf = m;
     	str = s;
-//    	this.setName(s);
-//    	this.setLayout(new BorderLayout());
-//    	
-        JPanel ui = new JPanel(/*new BorderLayout(4,4)*/);
-//        ui.setBorder(new EmptyBorder(4,4,4,4));
+    	String block = "0";
+    	this.setName(s);
+    	this.setLayout(new BorderLayout());
+    	
+        JPanel ui = new JPanel(new BorderLayout(4,4));
+        ui.setBorder(new EmptyBorder(4,4,4,4));
         this.add(ui);
-////    	
-////        selectedSeats.setLineWrap(true);
-////        selectedSeats.setWrapStyleWord(true);
-////        selectedSeats.setEditable(false);
-////        ui.add(new JScrollPane(selectedSeats), BorderLayout.PAGE_END);
-////
-////        JPanel sitzplatz = new JPanel(new BorderLayout(40, 0));
-////        sitzplatz.setBorder(new TitledBorder("Available Seats"));
-////        ui.add(sitzplatz, BorderLayout.CENTER);
-////        
-////        JPanel leftStall = new JPanel(new GridLayout(0, 2, 2, 2));
-////        JPanel centerStall = new JPanel(new GridLayout(50,10));
-////        JPanel rightStall = new JPanel(new GridLayout(0, 2, 2, 2));
-////        JPanel upStall = new JPanel(new BorderLayout());
-////        JPanel downStall = new JPanel();
-////        
-////        sitzplatz.add(leftStall, BorderLayout.WEST);
-////        sitzplatz.add(centerStall, BorderLayout.CENTER);
-////        sitzplatz.add(rightStall, BorderLayout.EAST);
-////        sitzplatz.add(upStall, BorderLayout.NORTH);
-////        sitzplatz.add(downStall, BorderLayout.SOUTH);
-////		
-////		JPanel seatdetails = new JPanel();
-////		upStall.add(seatdetails, BorderLayout.CENTER);
-////		JLabel testdetail = new JLabel("test");
-////		seatdetails.add(testdetail);
-////
-////        ActionListener seatSelectionListener = new ActionListener() {
-////            @Override
-////            public void actionPerformed(ActionEvent e) {
-////                showSelectedSeats();
-////            }
-////        };
-////
-////        for (int ii=0; ii <seats.length; ii++) {
-////            JToggleButton tb = new JToggleButton("-" + (ii+1));
-////            tb.addActionListener(seatSelectionListener);
-////            seats[ii] = tb;
-////            centerStall.add(tb);
-////        }
-//
-////    private void showSelectedSeats() {
-////        StringBuilder sb = new StringBuilder();
-////        for (int ii=0; ii<seats.length; ii++) {
-////            JToggleButton tb = seats[ii];
-////            if (tb.isSelected()) {
-////                sb.append(tb.getText());
-////                sb.append(", ");
-////            }
-////        }
-////        String s = sb.toString();
-////        if (s.length()>0) {
-////            s = s.substring(0, s.length()-2);
-////        }
-////        selectedSeats.setText(s);
-////    }
-//    
-//    public void pc(String str){
-//		mf.PanelChange((JPanel)this, str);
+    	
+        selectedSeats.setLineWrap(true);
+        selectedSeats.setWrapStyleWord(true);
+        selectedSeats.setEditable(false);
+        ui.add(new JScrollPane(selectedSeats), BorderLayout.PAGE_END);
+        
+		JPanel seatdetails = new JPanel();
+		ui.add(seatdetails, BorderLayout.NORTH);
+		JLabel testdetail = new JLabel("Gewünschte Block Nr");
+		seatdetails.add(testdetail);
+		
+		String[] blockdata = {"","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
+		JComboBox blockBox = new JComboBox(blockdata);
+		seatdetails.add(blockBox);
+		int selectedBlock = blockBox.getSelectedIndex();
+		blockBox.addActionListener( new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e){
+				StringBuilder sb = new StringBuilder();
+		        for (int ii=0; ii<seats.length; ii++) {
+		            JToggleButton tb = seats[ii];
+		            if (tb.isSelected()) {
+		                sb.append(tb.getText());
+		                sb.append(", ");
+		            }
+		        }
+		        String s = sb.toString();
+		        if (s.length()>0) {
+		            s = s.substring(0, s.length()-2);
+		        }
+		        selectedSeats.setText(String.valueOf(selectedBlock)+s);
+			}
+		});
+		
+		
+        JPanel sitzplatz = new JPanel(new BorderLayout(40, 0));
+        sitzplatz.setBorder(new TitledBorder("Available Seats"));
+        ui.add(sitzplatz, BorderLayout.CENTER);
+        
+        JPanel leftStall = new JPanel(new GridLayout(0, 2, 2, 2));
+        JPanel centerStall = new JPanel(new GridLayout(25,20));
+        JPanel rightStall = new JPanel(new GridLayout(0, 2, 2, 2));
+        JPanel upStall = new JPanel(new BorderLayout());
+        JPanel downStall = new JPanel();
+        
+        sitzplatz.add(leftStall, BorderLayout.WEST);
+        sitzplatz.add(centerStall, BorderLayout.CENTER);
+        sitzplatz.add(rightStall, BorderLayout.EAST);
+        sitzplatz.add(upStall, BorderLayout.NORTH);
+        sitzplatz.add(downStall, BorderLayout.SOUTH);
+	
+
+        ActionListener seatSelectionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showSelectedSeats();
+            }
+        };
+
+        for (int ii=0; ii <seats.length; ii++) {
+            JToggleButton tb = new JToggleButton(ii+1+"");
+            tb.addActionListener(seatSelectionListener);
+            seats[ii] = tb;
+            centerStall.add(tb);}
+        }
+
+    private void showSelectedSeats() {
+        StringBuilder sb = new StringBuilder();
+        for (int ii=0; ii<seats.length; ii++) {
+            JToggleButton tb = seats[ii];
+            if (tb.isSelected()) {
+                sb.append(tb.getText());
+                sb.append(", ");
+            }
+        }
+        String s = sb.toString();
+        if (s.length()>0) {
+            s = s.substring(0, s.length()-2);
+        }
+        selectedSeats.setText(s);
+    }
+    
+    public void pc(String str){
+		mf.PanelChange((JPanel)this, str);
 }
     
 }
